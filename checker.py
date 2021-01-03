d6d7f72a45d882c89ff11ff7045440c767f92e77
Rollly <31448610+rollly00@users.noreply.github.com>
2021-01-03 17:50:21 +0600

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import datetime
+import time
+token = "TOKEN"
+check = ""
+while True:
+	cookie = dict(sessionid='INSTAGRAM COOKIE')
+	req = requests.get('https://i.instagram.com/api/v1/direct_v2/get_presence/', cookies=cookie, headers={'X-IG-App-ID': '936619743392459'}).json()
+	status = req['user_presence']['USER ID']['last_activity_at_ms']
+	date = datetime.datetime.fromtimestamp(status/1000).strftime('%H:%M %d.%m.%Y')
+	if date != check:
+		check = date
+		requests.get('https://api.telegram.org/bot'+ token + '/sendMessage?chat_id=CHATID&text=' + 'LAST ACTIVITY:\n' + check)
